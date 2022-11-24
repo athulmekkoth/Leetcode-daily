43e13e2ee0a3c7fec0c482df33f4ca5d13508ee5
ATHUL SURESH <68680001+athulmekkoth@users.noreply.github.com>
2022-11-24 18:09:30 +0530

    Time: 165 ms (45.57%), Space: 23.7 MB (74.13%) - LeetHub

--- 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
+++ 1277-count-square-submatrices-with-all-ones/1277-count-square-submatrices-with-all-ones.cpp
@@ -6,21 +6,20 @@ public:
         {
             for(int j=0;j<matrix[0].size();j++)
             {
-                
-                if(i > 0 && j > 0 && matrix[i][j]>0)
-                    matrix[i][j] = min(matrix[i-1][j-1], min(matrix[i-1][j], matrix[i][j-1])) + 1;
-                
-                result += matrix[i][j];
-                /*if(i>0 && j>0 && matrix[i][j]>0)
-                {                    matrix[i][j] = min(matrix[i-1][j-1], min(matrix[i-1][j], matrix[i][j-1])) + 1;
-
-                   // matrix[i][j]=min(matrix[i-1][j-1],min(matrix[i-1][j],matrix[i][j-1]))+1;
-                    result+=matrix[i][j];
-                }*/
-            }
+                                 
+            if(i >0 && j>0 &&matrix[i][j]>0){
+                   
+                   matrix[i][j] = min(matrix[i-1][j-1],min(matrix[i-1][j],matrix[i][j-1]))+1;
+              // matrix[i][j] = min(matrix[i-1][j-1], min(matrix[i-1][j], matrix[i][j-1])) + 1;
             
+ 
+            }
+                    
+                result += matrix[i][j];
+            }
         }
         return result;
         
+
     }
 };
